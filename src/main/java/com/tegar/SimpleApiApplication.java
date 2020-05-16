20ec73696d412a81ee70e04b7ad91fccc1bad350
tegarjokam <dtegarjokam354@gmail.com>
2020-05-16 15:27:45 +0700

    init

---
+++ src/main/java/com/tegar/SimpleApiApplication.java
@@ -0,0 +1,15 @@
+package com.tegar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class SimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiApplication.class, args);
+	}
+
+}
